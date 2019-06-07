cfac60914f93e8c92f11f7d8df7ca4e9dbcd3803
Aakaash Jois <aakaashjois@gmail.com>
2019-06-07 16:49:16 -0400

    Add code to parse numbers, and generate all word and number combinations

--- wordify/src/wordify.py
+++ wordify/src/wordify.py
@@ -1,13 +1,15 @@
 import string
+import itertools
 
 
 class Wordify:
 
     def __init__(self, number: str) -> None:
         self.has_country_code = False
-        self.combinations = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs',
-                             'tuv', 'wxyz']
+        self.combinations = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno',
+                             'pqrs', 'tuv', 'wxyz']
         self.number = number
+        self.all_perms = list()
 
     def preprocess_number(self) -> str:
         number = self.number.translate(str.maketrans('', '',
@@ -20,8 +22,23 @@ class Wordify:
                 '{} is not a valid US phone number.'.format(number))
         return number
 
+    def get_all_words(self):
+        num_list = self.parse_number()
+        comb = [self.combinations[num] for num in num_list]
+        temp = [comb[0]]
+        for i in range(1, len(comb)):
+            temp.append(comb[i])
+            self.all_perms = self.all_perms + list(map(lambda x: ''.join(x),
+                                                       itertools.product(
+                                                           *temp)))
+
+    def parse_number(self):
+        return list(map(int, self.number))
+
 
 if __name__ == '__main__':
-    ph_num = '1800734653'
+    ph_num = '18007246837'
     wordify = Wordify(ph_num)
-    print(wordify.preprocess_number())
+    wordify.preprocess_number()
+    wordify.get_all_words()
+    print(wordify.all_perms)
